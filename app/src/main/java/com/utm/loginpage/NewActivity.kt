7ca0b490151de71555961dbc5270c0af8375fd0a
Vinh Dao <86164573+snuzj@users.noreply.github.com>
2023-05-07 11:20:29 +0700

    Initial commit

---
+++ app/src/main/java/com/utm/loginpage/NewActivity.kt
@@ -0,0 +1,12 @@
+package com.utm.loginpage
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class NewActivity:  AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new)
+    }
+
+}
